6fd76d14a47142b69f95f02eac6aafe7425e9699
pome95 <pome95@kakao.com>
2021-01-06 13:06:49 +0900

    큐 기초

--- (2)Queue_test.py
+++ (2)Queue_test.py
@@ -23,6 +23,9 @@ data_queue.qsize()
 
 # => 3
 
+# ## FIFO(First In First Out) 방식  - 가장 먼저 넣은 것이 가장 먼저 나온다.
+
+
 
 
 
@@ -42,7 +45,7 @@ data_queue.qsize()
 data_queue.get()
 
 # => 1
-# ##### Last in First out에 맞게 나중에 넣은 것이 먼저 나온다 => 스택구조
+# ##### LIFO(Last In First Out) 방식 - 가장 마지막에 넣은 것이 가장 먼저 나온다.(스택구조)
 
 
 
@@ -109,8 +112,7 @@ dequeue()
 
 # => 1
 
-dequeue()
 
-# => 2
-
+# ## enqueue는 append를 통해 data를 입력 => 반복문을 통해 10개 숫자 자동 입력
+# ## dequeue는 queue_list(생성된 큐)의 첫째를 출력하고 삭제
 # ## Enqueue로 데이터를 넣고 Dequeue로 데이터를 뼤고
