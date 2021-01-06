a4ed72f5c75ab857d28097d20bc61da333d213b6
pome95 <pome95@kakao.com>
2021-01-06 13:06:24 +0900

    배열 기초

--- (1)Array_test.py
+++ (1)Array_test.py
@@ -20,18 +20,38 @@ print(data)
 
 # => [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-print(data_list[0])
+print(data[0])
 
 # => [1,2,3]
 
-print(data_list[2])
 
-# => [7,8,9]
+print(data[0][0])
+print(data[0][1])
+print(data[0][2])
+print(data[1][0])
+print(data[1][1])
+print(data[1][2])
+print(data[2][0])
+print(data[2][1])
+print(data[2][2])
 
-print(data_list[2][2],data_list[2][1],data_list[2][0])
+# => 1 2 3 4 5 6 7 8 9
 
-# => 9 8 7
 
+
+# #4X4 배열 만들고 출력하기 (반복문)
+data = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+for i in range(len(data)):
+    for j in range(len(data[i])):
+        print(data[i][j]
+
+# => 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+              
+# ## i는 len(data)만큼 -> data의 길이만큼(4)
+# ## j는 len(data[i])만큼 -> 반복된 i안의 요소 길이 만큼 (4) -> 4x4 행렬
+              
+              
+              
 
 
 dataset = ['Braund, Mr. Owen Harris',
