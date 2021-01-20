9c52d28d9b0f3d2fcb951c98a96421b125031029
Junho Han <pome95@kakao.com>
2021-01-20 23:04:43 +0900

    Update hash_table.py

--- Hash_table/hash_table.py
+++ Hash_table/hash_table.py
@@ -6,15 +6,17 @@
 # ### 기본 해쉬 테이블 생성과 데이터 입력 및 출력해보기
 # #### 파이썬 list comprehension 사용
 
+# 파이썬 list()를 사용하여 10칸의 테이블을 생성
 hash_table = list([0 for i in range(10)])
 
-def hash_func(key):
-  return key % 5
-  
+# 데이터 입력
 data1='Andy'
 data2='Dave'
 data3='Trump'
 
+# 데이터 출력
 print(ord(data1[0]),ord(data2[0]),ord(data3[0]))
 
+# ord() => 문자의 ASCII 코드를 리턴해주는 함수
+ 
 # Return => 65 68 84 
