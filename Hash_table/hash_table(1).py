01a47be1f5c7805858f1996b9f206d59a0db30db
pome95 <pome95@kakao.com>
2021-01-23 15:45:13 +0900

    해쉬 테이블 기초(1)

--- Hash_table/hash_table(1).py
+++ Hash_table/hash_table(1).py
@@ -25,7 +25,7 @@ print(ord(data1[0]),ord(data2[0]),ord(data3[0]))
 # 이런 방식으로 가능
 # -----------------------------------------
 
-# 해쉬 테이블 작성해보기
+# 함수작성으로 해쉬 테이블 작성해보기
 
 hash_table = list(0 for i in range(10))
 
@@ -56,39 +56,6 @@ get_data('Andy')
 
 # --------------------------------------
 
-# 코드 간결하게 
-hash_table=list([0 for i in range(8)]) # list comprehension 사용 => 8칸 테이블 생성
-
-# 키값을 가져오는 함수
-def get_key(data):
-    return hash(data)
-
-# 해싱 함수 => Division법 %8 사용
-def hash_function(key):
-    return key % 8
-
-# 데이터 저장 부분
-def save_data(data,value):
-    hash_address = hash_function(get_key(data))
-    hash_table[hash_address] = value
-
-# 데이터 출력 부분   
-def read_data(data):
-    hash_address = hash_function(get_key(data))
-    return hash_table[hash_address]
-  
-
-save_data('Dave','0102030200')
-save_data('Andy','01033232200')
-
-read_data('Dave')
-# Result => '0102030200'
-# 데이터가 테이블에 들어간 것을 확인가능
-
-hash_table
-
-# Result => [0, 0, 0, '0102030200', 0, 0, 0, '01033232200']
-# 해당 데이터가 Division(나머지) 연산에 의해 자리에 들어간 것 확인 가능
 
 
 
