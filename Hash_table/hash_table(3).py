6dc2662217abcc6daaee763f5c516f502dd3b568
Junho Han <pome95@kakao.com>
2021-01-25 21:00:30 +0900

    해시 테이블 기초(3)

--- Hash_table/hash_table(3).py
+++ Hash_table/hash_table(3).py
@@ -37,5 +37,21 @@ def read_data(data):
         return None
     else:
         return None
-                                     
+
+# 중복된 곳에 값을 넣기 위해 임의의 값 출력해서 같은 값이 나오는 두 데이터를 찾아보기                                         
+print (hash('Dave') % 8)
+print (hash('Dd') % 8)
+print (hash('Data') % 8)
                                          
+# Result => 0 5 5
+# 나누기 결과 5로 같은 결과가 나오는 것을 확인                                         
+                                         
+save_data('Dd', '1201023010')
+save_data('Data', '3301023010')
+read_data('Dd')
+                            
+# Result => '1201023010'
+                                         
+hash_table
+                                         
+# Result => [0,0,0,0,0,[[5532289232361108405, '1201023010'], [872982679201566045, '3301023010']],0,0]                                      
