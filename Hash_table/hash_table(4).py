5c6cdfc59d624cfd188f94bd580047b2ea3a03dd
pome95 <pome95@kakao.com>
2021-02-03 21:26:26 +0900

    Change files

--- Hash_table/hash_table(4).py
+++ Hash_table/hash_table(4).py
@@ -3,7 +3,7 @@
 
 # ## 해시 테이블 (Hash Table)
 
-# ### 해시 테이블 충돌 해결 (Chaining)
+# ### 해시 테이블 충돌 해결 (Linear Probing)
 
 hash_table=list([0 for i in range(8)]) # list comprehension 사용 => 8칸 테이블 생성
 
@@ -19,24 +19,29 @@ def save_data(data,value):
     index_key = get_key(data)
     hash_address = hash_function(index_key)
     if hash_table[hash_address]!=0:
-        for index in range(len(hash_table[hash_address])):
-            if hash_table[hash_address][index][0] == index_key:
-                hash_table[hash_address][index][1] = value
+        for index in range(hash_address,len(hash_table)):
+            if hash_table[index]==0:
+                hash_table[index] = [index_key,value]
                 return
-        hash_table[hash_address].append([index_key,value])
+            elif hash_table[index][0] == index_key:
+                hash_table[index][1] = value
+                return
     else:
-        hash_table[hash_address] = [[index_key,value]]
+        hash_table[hash_address]=[index_key,value]
 
 def read_data(data):
     index_key = get_key(data)
     hash_address = hash_function(index_key)
     if hash_table[hash_address]!=0:
-        for index in range(len(hash_table[hash_address])):
-            if hash_table[hash_address][index][0] == index_key:
-               return hash_table[hash_address][index][1]
-        return None
+        for index in range(hash_address, len(hash_table)):
+            if hash_table[index]==0:
+                return None
+            elif hash_table[index][0] == index_key:
+                return hash_table[index][1]
     else:
         return None
+
+
 
 # 중복된 곳에 값을 넣기 위해 임의의 값 출력해서 같은 값이 나오는 두 데이터를 찾아보기                                         
 print (hash('Dave') % 8)
@@ -54,4 +59,5 @@ read_data('Dd')
                                          
 hash_table
                                          
-# Result => [0,0,0,0,0,[[5532289232361108405, '1201023010'], [872982679201566045, '3301023010']],0,0]                                      
+# Result => [0,0,0,0,0,[5532289232361108405, '1201023010'], [872982679201566045, '3301023010'],0]        
+# 'Dd'의 값이 5번째 테이블에 들어가고 'Data' 값이 5번째가 아닌 그 다음번째에 들어간 것 확인                              
